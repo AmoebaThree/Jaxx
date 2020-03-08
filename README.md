914d71c9d31c7062c67eb75801663ad96440da11
chrisalexander <chris@chris-alexander.co.uk>
2020-03-08 20:13:05 +0000

    Update defaults, less breakage

--- README.md
+++ README.md
@@ -16,10 +16,8 @@ Format: \<channel> "message"
 
 * \<jaxx> "\<pan_angle>,\<pitch_angle>"
   * Points the head at the specified pan and pitch angles
-  * Note that 0,0 is dead ahead
-  * Pan angle range: +- 80
-  * Pitch angle range: +80 to -25
-  * Dead ahead: 0,10
+  * Note that 0,10 is dead ahead, but you can just publish `jaxx 0` to get there
+  * Pan angle range: +- 80; Pitch angle range: +80 to -25; these are enforced in the code
 
 **Outputs**
 
--- jaxx.py
+++ jaxx.py
@@ -1,10 +1,15 @@
 if __name__ == '__main__':
-    import systemd.daemon, initio, redis
+    import systemd.daemon
+    import initio
+    import redis
+
+    x = {'init': 0, 'max': 80, 'min': -80}
+    y = {'init': 10, 'max': 80, 'min': -25}
 
     print('Startup')
     initio.init(Servos=True)
-    initio.setServo(0, 0)
-    initio.setServo(1, 0)
+    initio.setServo(0, x['init'])
+    initio.setServo(1, y['init'])
     r = redis.Redis(host='192.168.0.1', port=6379, db=0)
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe('jaxx')
@@ -14,9 +19,27 @@ if __name__ == '__main__':
     try:
         for message in p.listen():
             cmd = message["data"]
-            angles = cmd.split(',')
-            initio.setServo(0, int(angles[0]))
-            initio.setServo(1, int(angles[1]))
+
+            if cmd == '0':
+                x_angle = x['init']
+                y_angle = y['init']
+            else:
+                angles = cmd.split(',')
+                x_angle = int(angles[0])
+                y_angle = int(angles[1])
+
+            # Make sure it's valid
+            if x_angle > x['max']:
+                x_angle = x['max']
+            if x_angle < x['min']:
+                x_angle = x['min']
+            if y_angle > y['max']:
+                y_angle = y['max']
+            if y_angle < y['min']:
+                y_angle = y['min']
+
+            initio.setServo(0, x_angle)
+            initio.setServo(1, y_angle)
     except:
         p.close()
         initio.setServo(0, 0)
